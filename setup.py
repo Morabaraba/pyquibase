71bc871207a4aa293b049dc76fc1cbbff15131af
Eun Song <rampart81@gmail.com>
2017-07-21 15:48:36 +0900

    Bumped up the version to 1.0

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'pyquibase',
-    version          = '0.5',
+    version          = '1.0',
     description      = 'Python wrapper for liquibase',
     author           = 'Eun Woo Song',
     author_email     = 'songew@gmail.com',
     url              = 'https://github.com/rampart81/pyquibase',
-    download_url     = 'https://githur.com/rampart81/pyquibase/archive/0.5.tar.gz',
+    download_url     = 'https://githur.com/rampart81/pyquibase/archive/1.0.tar.gz',
     install_requires = [ ],
     packages         = find_packages(exclude = ['docs', 'tests*']),
     keywords         = ['liquibase', 'db migration'],
